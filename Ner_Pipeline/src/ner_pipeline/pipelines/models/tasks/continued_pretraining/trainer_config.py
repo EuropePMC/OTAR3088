d436d31cad51c85b0b9f3ee917fa805c61b140db
amina-mardiyyah <amina.rufai@yahoo.com>
2026-09-24 11:58:35 +0100

    remove old MLM scripts

--- Ner_Pipeline/src/ner_pipeline/pipelines/models/tasks/continued_pretraining/trainer_config.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-
-# @dataclass(frozen=True)
-# class TaptTrainerKwargs(BaseTrainerKwargs):
-#     data_collator: Union[DataCollatorForLanguageModeling, DataCollatorForWholeWordMask]
-#     preprocess_logits_for_metrics: Callable
